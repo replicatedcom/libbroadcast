ed61c424fa2fefc7acd97a90534c1d205b973ff9
Joe Toscano <jtoscano42@gmail.com>
2015-04-10 17:43:38 -0700

    Update code

--- broadcaster.go
+++ broadcaster.go
@@ -1,4 +1,4 @@
-package libbroadcast
+package eventmanager
 
 type Broadcaster struct {
 	listeners map[string]chan interface{}
@@ -10,36 +10,36 @@ func newBroadcaster() *Broadcaster {
 	}
 }
 
-func (broadcaster *Broadcaster) Listen(id string, onBroadcast func(interface{})) {
-	listener := make(chan interface{})
-	broadcaster.listeners[id] = listener
-
+func (broadcaster *Broadcaster) GetNextMessage(id string, onBroadcast func(interface{})) {
+	listener := broadcaster.GetChan(id)
 	go func() {
 		data := <-listener
+		broadcaster.RemoveListener(id)
 		onBroadcast(data)
 	}()
 }
 
-func (broadcaster *Broadcaster) ListenSynchronously(id string) interface{} {
-	listener := make(chan interface{})
-	broadcaster.listeners[id] = listener
-
+func (broadcaster *Broadcaster) GetNextMessageSynchronously(id string) interface{} {
+	listener := broadcaster.GetChan(id)
 	data := <-listener
+	broadcaster.RemoveListener(id)
 	return data
 }
 
 func (broadcaster *Broadcaster) GetChan(id string) chan interface{} {
-	listener := make(chan interface{})
+	listener := make(chan interface{}, 1)
 	broadcaster.listeners[id] = listener
 	return listener
 }
 
-func (broadcaster *Broadcaster) Send(data interface{}) {
-	for _, listener := range broadcaster.listeners {
-		listener <- data
-	}
-}
-
 func (broadcaster *Broadcaster) RemoveListener(id string) {
 	delete(broadcaster.listeners, id)
+}
+
+func (broadcaster *Broadcaster) Send(data interface{}) {
+	for _, listener := range broadcaster.listeners {
+		go func(l chan interface{}) {
+			l <- data
+		}(listener)
+	}
 }
