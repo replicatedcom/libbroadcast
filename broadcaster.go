f36b4b7abcbc6adc1fb43dd145e70c67822e382d
divolgin <dmitriy@replicated.com>
2016-03-28 13:53:05 -0700

    Locks around broadcaster map access

--- broadcaster.go
+++ broadcaster.go
@@ -2,18 +2,21 @@ package libbroadcast
 
 import (
 	"log"
+	"sync"
 	"time"
 )
 
 type Broadcaster struct {
 	name      string
 	listeners map[string]chan interface{}
+	mutex     *sync.Mutex
 }
 
 func newBroadcaster(name string) *Broadcaster {
 	return &Broadcaster{
 		name:      name,
 		listeners: make(map[string]chan interface{}),
+		mutex:     &sync.Mutex{},
 	}
 }
 
@@ -28,20 +31,31 @@ func (broadcaster *Broadcaster) GetNextMessage(id string, onBroadcast func(inter
 
 func (broadcaster *Broadcaster) GetChan(id string) chan interface{} {
 	listener := make(chan interface{}, 1)
+
+	broadcaster.mutex.Lock()
+	defer broadcaster.mutex.Unlock()
 	broadcaster.listeners[id] = listener
+
 	return listener
 }
 
 func (broadcaster *Broadcaster) RemoveListener(id string) {
+	broadcaster.mutex.Lock()
+	defer broadcaster.mutex.Unlock()
 	delete(broadcaster.listeners, id)
 }
 
 func (broadcaster *Broadcaster) HasListeners() bool {
+	broadcaster.mutex.Lock()
+	defer broadcaster.mutex.Unlock()
 	return len(broadcaster.listeners) > 0
 }
 
 func (broadcaster *Broadcaster) Send(data interface{}) {
+	broadcaster.mutex.Lock()
+	defer broadcaster.mutex.Unlock()
 	for id, listener := range broadcaster.listeners {
+		// Don't do anything synchronously here becasue we are holding the lock
 		go func(i string, l chan interface{}) {
 			select {
 			case l <- data:
